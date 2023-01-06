9f1382698ffcc4d2b2e0a9c88c21d7b2668e5594
Michael Hofer <hoferm90@gmail.com>
2023-01-06 16:52:08 +0100

    Switch window manager to dwm

---
+++ bin/dwm/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+feh --bg-fill ~/.local/share/d/pictures/wallpaper/Street.jpg &
+xset r rate 200 50 &
+pipewire &
+wireplumber &
+picom &
+
+~/bin/dwm/bar.sh &
+while type dwm >/dev/null; do dwm && continue || break; done
