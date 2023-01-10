f1b3de0838eeef7adb2de271902b92293d493dc1
Michael Hofer <hoferm90@gmail.com>
2023-01-10 23:55:17 +0100

    startx: Change pipewire start order

--- bin/dwm/run.sh
+++ bin/dwm/run.sh
@@ -3,8 +3,8 @@
 feh --bg-fill ~/.local/share/d/pictures/wallpaper/Street.jpg &
 xset r rate 200 50 &
 # pipewire &
-# wireplumber &
 picom &
+wireplumber &
 
 ~/bin/dwm/bar.sh &
 while type dwm >/dev/null; do dwm && continue || break; done
